bfed62e3712e4c164ca62c10f857111538c3b3e6
meera-greycube <meera@greycube.in>
2024-12-17 13:18:43 +0000

    feat: change label of Go To Report button in payroll entry

--- xtc/public/js/payroll_entry.js
+++ xtc/public/js/payroll_entry.js
@@ -1,6 +1,6 @@
 frappe.ui.form.on('Payroll Entry', {
     refresh: function(frm) {
-        frm.add_custom_button(__('Go to Report'), function() {
+        frm.add_custom_button(__('DBS SAL'), function() {
             frappe.set_route('query-report', 'DBS SAL file', {
                 payroll_entry: frm.doc.name
             });
